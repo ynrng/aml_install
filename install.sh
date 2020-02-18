0f7ada0faaca265f2d2b8b17cb67b258d55167f2
JustaGist <saifksidhik@gmail.com>
2020-02-18 11:05:41 +0000

    test

--- install.sh
+++ install.sh
@@ -21,64 +21,64 @@ fi
 BRANCH="melodic-dev"
 
 # echo "$0"
-echo "$1"
-echo "$2"
+# echo "$1"
+# echo "$2"
 
-# while [[ $# -gt 0 ]]; do
+while [[ $# -gt 0 ]]; do
 
-# 	key="$1"
-# 	value="$2"
+	key="$1"
+	value="$2"
 
-# 	echo $key
-# 	echo $value
-# 	echo " "
-#     case $key in
-#         -w|--workspace)
-#         WORKSPACE_PATH="$value"
-#         shift # past argument
-#         shift # past value
-#         ;;
-#         -b|--aml_branch)
-#         BRANCH="$value"
-#         shift # past argument
-#         shift # past value
-#         ;;   
-#         *)    # unknown option
-#         INSTALL_TYPE+=("$key") # save it in an array for later
-#         shift # past argument
-#         ;;
-#     esac
-# done
+	echo $key
+	echo $value
+	echo " "
+    case $key in
+        -w|--workspace)
+        WORKSPACE_PATH="$value"
+        shift # past argument
+        shift # past value
+        ;;
+        -b|--aml_branch)
+        BRANCH="$value"
+        shift # past argument
+        shift # past value
+        ;;   
+        *)    # unknown option
+        INSTALL_TYPE+=("$key") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
+done
 
-# echo "INSTALL_TYPE: $INSTALL_TYPE"
-# echo "WORKSPACE_PATH: $WORKSPACE_PATH"
-# echo "GIT BRANCH: $BRANCH"
-
-
-# if [ "$INSTALL_FROM_HOST" == "true" ] && [ -z $WORKSPACE_PATH ]
-# then
-# 	echo "WORKSPACE_PATH is empty: will prompt to use default, i.e. aml_ws"
-# fi
+echo "INSTALL_TYPE: $INSTALL_TYPE"
+echo "WORKSPACE_PATH: $WORKSPACE_PATH"
+echo "GIT BRANCH: $BRANCH"
 
 
-# if [ "$INSTALL_FROM_HOST" == "true" ]
-# then
-# 	echo "Local install..."
-# 	ROOT_DIR="$(cd $( dirname ${BASH_SOURCE[0]} ) && pwd)"
-# 	${ROOT_DIR}/install_on_host.sh $BRANCH $WORKSPACE_PATH
-# else
-# 	if [ -z "$INSTALL_TYPE" ] || [ "$INSTALL_TYPE" == "bash" ]
-# 	then 
-# 	    echo "usage: ./install.sh <install-options> <image-type>"
-# 	    echo "example: ./install.sh --workspace $HOME/Projects/ melodic-dev"
-# 	    echo "install options: --branch: AML git branch"
-# 	    echo "                 --workspace: absolute path to create workspace (Default: $HOME/Projects/aml_ws)"
-# 		exit 1
-# 	fi
-# 	echo "Curl-based install..."
-# 	rm -rf /tmp/aml_install
-# 	git clone --depth 1 -b master https://github.com/justagist/aml_install.git /tmp/aml_install
-# 	cd /tmp/aml_install
-# 	./install_docker.sh $INSTALL_TYPE $BRANCH $WORKSPACE_PATH
-# fi
+if [ "$INSTALL_FROM_HOST" == "true" ] && [ -z $WORKSPACE_PATH ]
+then
+	echo "WORKSPACE_PATH is empty: will prompt to use default, i.e. aml_ws"
+fi
+
+
+if [ "$INSTALL_FROM_HOST" == "true" ]
+then
+	echo "Local install..."
+	ROOT_DIR="$(cd $( dirname ${BASH_SOURCE[0]} ) && pwd)"
+	${ROOT_DIR}/install_on_host.sh $BRANCH $WORKSPACE_PATH
+else
+	if [ -z "$INSTALL_TYPE" ] || [ "$INSTALL_TYPE" == "bash" ]
+	then 
+	    echo "usage: ./install.sh <install-options> <image-type>"
+	    echo "example: ./install.sh --workspace $HOME/Projects/ melodic-dev"
+	    echo "install options: --branch: AML git branch"
+	    echo "                 --workspace: absolute path to create workspace (Default: $HOME/Projects/aml_ws)"
+		exit 1
+	fi
+	echo "Curl-based install..."
+	rm -rf /tmp/aml_install
+	git clone --depth 1 -b master https://github.com/justagist/aml_install.git /tmp/aml_install
+	cd /tmp/aml_install
+	./install_docker.sh $INSTALL_TYPE $BRANCH $WORKSPACE_PATH
+fi
 
